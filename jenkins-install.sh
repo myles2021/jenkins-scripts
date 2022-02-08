50674db1909985ecf6135b6c25302f977a18fee2
myles2021 <86742200+myles2021@users.noreply.github.com>
2022-02-08 16:24:04 +0000

    Create jenkins-install.sh

---
+++ jenkins-install.sh
@@ -0,0 +1,29 @@
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install -y openjdk-11-jdk 
+sudo apt install -y jenkins
+
+#register the Jenkins service
+sudo systemctl daemon-reload
+
+#start the Jenkins service
+sudo systemctl start jenkins
+
+#verify Jenkins has started
+sudo systemctl status jenkins
+
+#retrieve the password for Jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+#retrieve your public IP address
+curl ifconfig.me
+
+#Go to your public IP on 8000
+http://<publicIP>:8000
+
+# Enter the password retrieved earlier
+
+# Install suggested plugins
+
+# Create First Admin User
