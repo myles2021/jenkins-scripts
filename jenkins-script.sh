38833afdd600ebc91e5dfc1806b088392dbbf07a
myles2021 <86742200+myles2021@users.noreply.github.com>
2022-02-17 12:11:34 +0000

    Create jenkins-script.sh

---
+++ jenkins-script.sh
@@ -0,0 +1,5 @@
+# Create a public GitHub repository for this exercise, you can call it jenkins-freestyle-project.
+
+# Add a script to the repository called run.sh, with the contents:
+
+# echo 'Hello from run.sh!'
