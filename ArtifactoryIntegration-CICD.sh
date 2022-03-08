79fb4307367df9b2ca2520845d4c5e8820d36c44
myles2021 <86742200+myles2021@users.noreply.github.com>
2022-03-08 12:08:58 +0000

    Update ArtifactoryIntegration-CICD.sh

--- ArtifactoryIntegration-CICD.sh
+++ ArtifactoryIntegration-CICD.sh
@@ -1,10 +1,5 @@
 # Create Jenkins CICD Pipeline with Artifactory Integration for Build Artifact Management
 
-ssh -i ~/.ssh/978280188869.pem ec2-user@54.212.79.105
-
-chmod 600 ~/.ssh/978280188869.pem
-
-
 ssh -i /Path/To/Your/KeyPair.pem AMIUserName@YourIPv4Address
 
 chmod 600 /Path/To/Your/KeyPair.pem
